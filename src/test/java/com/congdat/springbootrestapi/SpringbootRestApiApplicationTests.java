21a8a844ffd42ae98b25a58a5ba7d6d578b66f08
DatTran <khiemtran21012000@gmail.com>
2023-06-08 23:20:24 +0700

    init

---
+++ src/test/java/com/congdat/springbootrestapi/SpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.congdat.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
