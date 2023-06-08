a6aa2c9d453aaec96fcd899df46527bf6902cfb2
DatTran <khiemtran21012000@gmail.com>
2023-06-08 23:40:28 +0700

    add Post Entity

---
+++ src/main/java/com/congdat/springbootrestapi/entity/Post.java
@@ -0,0 +1,25 @@
+package com.congdat.springbootrestapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+}
