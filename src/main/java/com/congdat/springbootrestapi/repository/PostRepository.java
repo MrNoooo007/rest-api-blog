d5252c9a74d13aefd97d3963c782a7af8dcdec95
DatTran <khiemtran21012000@gmail.com>
2023-06-15 23:19:20 +0700

    add full code

--- src/main/java/com/congdat/springbootrestapi/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.congdat.springbootrestapi.repository;
-
-import com.congdat.springbootrestapi.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
